f82a6f76ccace6e158154baec6703584346df225
shivamiitkcse <kshivam22@iitk.ac.in>
2024-06-28 15:07:38 +0530

    hello

---
+++ script.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+app.use(function(req , res , next){
+    console.log('middleware1 chala diya');
+    //but this willl case the loader to load a lot
+    next();
+    //the next() will be actually throwing this further
+
+});
+//before getting any route , you are going to make this run because this is actually middle ware
+
+app.get("/" , function(req , res){
+    res.send("kaise ho bhai log");
+});//route
+app.get("/login" , function(req, res){
+    res.send("yeh toh sach hai ki abhi practice chahiye");
+});
+app.get('/register', function(req, res){
+    // res.send("hello");
+
+
+    res.send("dekha hazaro dafa aapko");
+});
+app.listen(3000);
+// we are going to learn about the request and response handling
+
+//frontend backend and frontend
+
+// we would be learning how to use middleware here
+
+// we can understand the concept of the middlewares
+//jab bhi server request accept karta hai waha se route ke beech pahucne tak agar aap use beech mein rokte ho toh woh middle ware hota hai 
+  
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,18 @@
+
+// // Write to a file
+// fs.writeFile('hey.txt', 'hey main to kaise ho', function (err) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log('File written successfully');    
+// }});
+
+// fs.appendFile('hey.txt' ," main toh mast hoon" , function(err){
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log('kaam ho gaya hai ');
+
+//     }
+// });
